89b1c7ba97f7e48c8544f1022564d2e5f9410925
Bruno Cruz <bruno.cruz@m4u.com.br>
2017-03-23 11:08:54 -0300

    add refresh endpoint to update diff on release

--- controllers/api.js
+++ controllers/api.js
@@ -56,6 +56,31 @@ module.exports = function(app){
 
 			},
 		},
+		refresh:{
+			get: function(req,res){
+
+				app.models.Application.findOne({name: req.params.app_name},{_id:false }).then(function(application){
+
+
+					if (!application) {
+						errorHandler("application " + req.params.app_name +" not found", res, 404);
+						return;
+					}
+
+					app.models.Release.find({application:req.params.app_name},function(err, releases){
+	                	for (var i = 0; i < releases.length; i++){
+	                		releases[i].save();
+	                		console.log("release " + releases[i].name + " refreshed");
+	                	}
+	            	});
+
+					res.status(202);
+					res.end();
+
+				}).catch(err => errorHandler(err,res));
+
+			}
+		},
 		application:{
 
 			post: function(req,res){
--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ module.exports = function(app) {
 
 	app.get("/apps/:app_name", app.controllers.web.release.get);
 
+	app.get("/apps/:app_name/refresh", app.controllers.api.refresh.get);
+
 	app.post("/api/apps", app.controllers.api.application.post);
 	app.post("/api/apps/:app_name/releases", app.controllers.api.release.post);
 	app.get("/api/apps/:app_name/releases", app.controllers.api.release.get);
